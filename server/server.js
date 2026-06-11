724ced532700af46e78ada9706acadee419c698c
Divyam Aggarwal <divyam@fyndbridge.in>
2026-06-11 16:59:56 +0530

    Load backend env from server directory

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+const path = require('path')
+
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 
 const { validateAiConfig } = require('./src/services/aiProvider')
 const app = require('./src/app')
