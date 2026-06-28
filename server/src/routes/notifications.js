1d5a113b00f3480ee6fe659b8690632d5f1ebc6e
Divyam Aggarwal <divyam@fyndbridge.in>
2026-06-29 00:17:29 +0530

    .

--- server/src/routes/notifications.js
+++ server/src/routes/notifications.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const controller = require('../controllers/notificationController')
+const { requireAdmin } = require('../middleware/adminAccessMiddleware')
 
 const router = express.Router()
 
 router.get('/', controller.listNotifications)
+router.post('/cleanup-old', requireAdmin, controller.cleanupOldNotifications)
 router.delete('/read', controller.clearReadNotifications)
 router.patch('/:id/read', controller.markNotificationRead)
 
 module.exports = router
-
