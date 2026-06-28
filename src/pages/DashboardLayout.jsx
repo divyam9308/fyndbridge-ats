c4e04ca125ca841cda3e5e430b5b13876057e17c
Divyam Aggarwal <divyam@fyndbridge.in>
2026-06-28 23:41:58 +0530

    .

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import Topbar from '../components/Topbar'
@@ -5,16 +6,23 @@ import './DashboardLayout.css'
 
 export default function DashboardLayout() {
   const location = useLocation()
+  const contentRef = useRef(null)
   const isDashboardEmbed = new URLSearchParams(location.search).get('embed') === 'dashboard'
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+    contentRef.current?.scrollTo?.({ top: 0, left: 0, behavior: 'auto' })
+  }, [location.pathname, location.search])
+
   if (isDashboardEmbed) {
-    return <main className="dashboard-content dashboard-embed" key={`${location.pathname}${location.search}`}><Outlet /></main>
+    return <main ref={contentRef} className="dashboard-content dashboard-embed" key={`${location.pathname}${location.search}`}><Outlet /></main>
   }
   return (
     <div className="dashboard-layout">
       <Sidebar />
       <div className="dashboard-main">
         <Topbar />
-        <main className="dashboard-content" key={location.pathname}>
+        <main ref={contentRef} className="dashboard-content" key={location.pathname}>
           <Outlet />
         </main>
       </div>
